e8438a471fc5b8c825bb514d2e72f63d22acd9f5
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-09-10 11:06:03 +0800

    init

---
+++ src/main/java/com/coba/jdbc/CobaJdbc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.coba.jdbc;
+
+/**
+ *
+ * @author novil
+ */
+public class CobaJdbc {
+    
+    public static void main(String[] args) {
+        Database db = new Database();
+    }
+}
